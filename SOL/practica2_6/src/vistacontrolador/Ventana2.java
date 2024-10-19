2b4fb83f4df0d5cdfe4cda149a5230727e707083
Kevin2001mol <kevin2001molina@gmail.com>
2024-10-19 13:39:32 +0200

    cerrar sesion operativo.
    falta el icono guay y ya de la parte 1.

--- SOL/practica2_6/src/vistacontrolador/Ventana2.java
+++ SOL/practica2_6/src/vistacontrolador/Ventana2.java
@@ -5,7 +5,7 @@ public class Ventana2 extends javax.swing.JFrame {
     public Ventana2(String nombre_usr) {
         initComponents();
 
-        bienvenido_lbl.setText(bienvenido_lbl.getText() + " " + nombre_usr);
+        logCorrectoLbl.setText(nombre_usr + " " + logCorrectoLbl.getText());
     }
 
     @SuppressWarnings("unchecked")
@@ -15,12 +15,20 @@ public class Ventana2 extends javax.swing.JFrame {
         panel_2 = new javax.swing.JPanel();
         bienvenido_lbl = new javax.swing.JLabel();
         cerrarSesion_button = new javax.swing.JButton();
+        logCorrectoLbl = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         bienvenido_lbl.setText("BIENVENID@");
 
         cerrarSesion_button.setText("Cerrar sesion");
+        cerrarSesion_button.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cerrarSesion_buttonActionPerformed(evt);
+            }
+        });
+
+        logCorrectoLbl.setText("se ha logueado correctamente.");
 
         javax.swing.GroupLayout panel_2Layout = new javax.swing.GroupLayout(panel_2);
         panel_2.setLayout(panel_2Layout);
@@ -33,14 +41,20 @@ public class Ventana2 extends javax.swing.JFrame {
                     .addGroup(panel_2Layout.createSequentialGroup()
                         .addGap(16, 16, 16)
                         .addComponent(bienvenido_lbl)))
-                .addContainerGap(162, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panel_2Layout.createSequentialGroup()
+                .addContainerGap(41, Short.MAX_VALUE)
+                .addComponent(logCorrectoLbl, javax.swing.GroupLayout.PREFERRED_SIZE, 326, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(21, 21, 21))
         );
         panel_2Layout.setVerticalGroup(
             panel_2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(panel_2Layout.createSequentialGroup()
                 .addGap(31, 31, 31)
                 .addComponent(bienvenido_lbl)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 168, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 112, Short.MAX_VALUE)
+                .addComponent(logCorrectoLbl)
+                .addGap(40, 40, 40)
                 .addComponent(cerrarSesion_button)
                 .addGap(50, 50, 50))
         );
@@ -63,7 +77,16 @@ public class Ventana2 extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void cerrarSesion_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cerrarSesion_buttonActionPerformed
+        Ventana1 ventana1 = new Ventana1();
+
+        ventana1.setVisible(true);
+
+        this.dispose();//cerrar la ventana 2
+    }//GEN-LAST:event_cerrarSesion_buttonActionPerformed
 
     public static void main(String args[]) {
         String nombreUsuario;
@@ -82,6 +105,7 @@ public class Ventana2 extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel bienvenido_lbl;
     private javax.swing.JButton cerrarSesion_button;
+    private javax.swing.JLabel logCorrectoLbl;
     private javax.swing.JPanel panel_2;
     // End of variables declaration//GEN-END:variables
 }
